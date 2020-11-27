d056a29150fca1dd28f8ec2adba2cc5f1c684955
Dominique Hazael-Massieux <dom@w3.org>
2020-11-27 18:24:21 +0100

    Set up github action to update data automatically

--- extract-data.js
+++ extract-data.js
@@ -1,11 +1,7 @@
 const fetch = require("node-fetch");
+const fs = require("fs").promises;
 
-const shortname = process.argv[2];
-const implfile = process.argv[3];
-let implData;
-if (implfile) {
-  implData = require(implfile);
-}
+const shortnames = require("./specs.json");
 const browsers = ['firefox', 'chrome', 'safari'];
 
 const idlreducer = (impldata, name) => (idlacc, member) => {
@@ -31,22 +27,21 @@ const idlreducer = (impldata, name) => (idlacc, member) => {
 };
 
 
-(async function(shortname, impldata) {
-  const specData = await fetch("https://w3c.github.io/webref/ed/idlparsed/" + shortname + ".json").then(r => r.json());
-  if (!impldata) {
-    impldata = await fetch("https://web-confluence.appspot.com/compatDAO:select", {method: "post", body: "{}", headers: {"Content-Type": "application/json"}}).then(r => r.json());
-  }
+(async function(shortnames) {
+  const impldata = await fetch("https://web-confluence.appspot.com/compatDAO:select", {method: "post", body: "{}", headers: {"Content-Type": "application/json"}}).then(r => r.json());
+  shortnames.forEach(async (shortname) =>  {
+    const specData = await fetch("https://w3c.github.io/webref/ed/idlparsed/" + shortname + ".json").then(r => r.json());
     const interfaces = Object.keys(specData.idlparsed.idlNames).filter(name => specData.idlparsed.idlNames[name].type === "interface").reduce((acc, name) => {
-    acc[name] = specData.idlparsed.idlNames[name].members.reduce(idlreducer(impldata, name), {});
-    return acc;
-  }, {});
-  Object.keys(specData.idlparsed.idlExtendedNames).reduce((acc, name) => {
-    specData.idlparsed.idlExtendedNames[name].forEach(partial => {
-      acc[name] = partial.members.reduce(idlreducer(impldata, name),  acc[name] || {});
+      acc[name] = specData.idlparsed.idlNames[name].members.reduce(idlreducer(impldata, name), {});
       return acc;
-    });
-    return acc;
-  }, interfaces);
-  console.log(JSON.stringify( interfaces, null, 2));
-
-})(shortname, implData);
+    }, {});
+    Object.keys(specData.idlparsed.idlExtendedNames).reduce((acc, name) => {
+      specData.idlparsed.idlExtendedNames[name].filter(i => i.type=== "interface").forEach(partial => {
+        acc[name] = partial.members.reduce(idlreducer(impldata, name),  acc[name] || {});
+        return acc;
+      });
+      return acc;
+                     }, interfaces);
+    await fs.writeFile(shortname + ".json", JSON.stringify( interfaces, null, 2));
+  });
+})(shortnames);
