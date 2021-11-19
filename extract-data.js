9b2931483ca26bebb1e57c66ba804a4b65b91ceb
Dominique Hazael-Massieux <dom@w3.org>
2021-11-19 14:24:54 +0100

    Deal with cases where WPT and TR shortnames differ

--- extract-data.js
+++ extract-data.js
@@ -8,16 +8,19 @@ const browsers = ['firefox', 'chrome', 'safari'];
   const wptruns = await fetch("https://wpt.fyi/api/runs?label=master").then(r => r.json());
   const wptdata = await fetch("https://wpt.fyi/api/search", {method: "post", body: `{"run_ids":${JSON.stringify(wptruns.map(r => r.id))}}`, headers: {"Content-Type": "application/json"}}).then(r => r.json());
   shortnames.forEach(async (shortname) =>  {
-    const idlData = (await fetch("https://w3c.github.io/webref/ed/idlparsed/" + shortname + ".json").then(r => r.json())).idlparsed;
+    const trShortname = shortname.tr ?? shortname;
+    const wptShortname = shortname.wpt ?? shortname;
+    const idlData = (await fetch("https://w3c.github.io/webref/ed/idlparsed/" + trShortname + ".json").then(r => r.json())).idlparsed;
     if (!idlData.idlNames) { idlData.idlNames = {}; }
     let interfaces = {results: {}};
     // Is there an idlharness test matching this shortname?
-    const testPath = wptdata.results.find(t => t.test.startsWith(`/${shortname}/idlharness`))?.test;
+    const testPath = wptdata.results.find(t => t.test.startsWith(`/${wptShortname}/idlharness`))?.test;
+    let impldata = [];
     if (!testPath) {
-      interfaces.error = `No idlharness tests found for ${shortname}`;
+      interfaces.error = `No idlharness tests found for ${trShortname}`;
     } else {
       // Fetch results for the said test
-      const impldata = await Promise.all(
+      impldata = await Promise.all(
         wptdata.runs
           .filter(r => browsers.includes(r.browser_name))
         // FIXME: URL hacking
@@ -27,52 +30,51 @@ const browsers = ['firefox', 'chrome', 'safari'];
             return {browser: {name: r.browser_name, version: r.browser_version}, results: await fetch(url).then(r => r.json())};
           })
       );
-      // gathering information on full and partial interfaces
-      const interfaceNames = Object.keys(idlData.idlNames).filter(name => idlData.idlNames[name].type === "interface").concat(
-        Object.keys(idlData.idlExtendedNames).filter(name => idlData.idlExtendedNames[name][0].type === "interface")
-      );
-      if (!interfaceNames.length) {
-        interfaces.error = `No interfaces found in ${shortname}`;
-      }
-      for (let interfaceName of interfaceNames) {
+    }
+    // gathering information on full and partial interfaces
+    const interfaceNames = Object.keys(idlData.idlNames).filter(name => idlData.idlNames[name].type === "interface").concat(
+      Object.keys(idlData.idlExtendedNames).filter(name => idlData.idlExtendedNames[name][0].type === "interface")
+    );
+    if (!interfaceNames.length) {
+      interfaces.error = `No interfaces found in ${trShortname}`;
+    }
+    for (let interfaceName of interfaceNames) {
         if (!interfaces.results[interfaceName]) {
           interfaces.results[interfaceName] = {};
         }
-        const interfaceList = [idlData.idlNames[interfaceName]].concat(idlData.idlExtendedNames[interfaceName] ?? []).filter(x => x);
-        let interfaceIsTested = false;
-        for (let interface of interfaceList) {
-          if (!interface.partial) {
-            // interface existence
-            interfaces.results[interfaceName].__interface = {};
-            for (let {browser, results} of impldata) {
-              let res = results.subtests.find(t => t.name === `${interfaceName} interface: existence and properties of interface object`);
-              if (res) {
+      const interfaceList = [idlData.idlNames[interfaceName]].concat(idlData.idlExtendedNames[interfaceName] ?? []).filter(x => x);
+      let interfaceIsTested = false;
+      for (let interface of interfaceList) {
+        if (!interface.partial) {
+          // interface existence
+          interfaces.results[interfaceName].__interface = {};
+          for (let {browser, results} of impldata) {
+            let res = results.subtests.find(t => t.name === `${interfaceName} interface: existence and properties of interface object`);
+            if (res) {
                 interfaceIsTested = true;
-                interfaces.results[interfaceName].__interface[browser.name] = res.status;
-              } else {
-                interfaces.results[interfaceName].__interface[browser.name] = "N/A";
-              }
+              interfaces.results[interfaceName].__interface[browser.name] = res.status;
+            } else {
+              interfaces.results[interfaceName].__interface[browser.name] = "N/A";
             }
-          } else {
-            // only check if the (partial) interface is included in the test
+          }
+        } else {
+          // only check if the (partial) interface is included in the test
+          if (impldata.length) {
             let {browser, results} = impldata[0];
             interfaceIsTested = results.subtests.find(t => t.name === `Partial interface ${interfaceName}: original interface defined`);
           }
-          // TODO: interface constructors?
+        }
+        // TODO: interface constructors?
         // interface members
-          if (interfaceIsTested) {
-            for (let member of (interface.members || []).filter(m => m.name)) {
-              interfaces.results[interfaceName][member.name] = {};
-              for (let {browser, results} of impldata) {
-                let res = results.subtests.find(t => t.name.startsWith(`${interfaceName} interface: ${member.type} ${member.name}`));
-                interfaces.results[interfaceName][member.name][browser.name] = res?.status || "N/A";
-              }
-
+        for (let member of (interface.members || []).filter(m => m.name)) {
+          interfaces.results[interfaceName][member.name] = {};
+            for (let {browser, results} of impldata) {
+              let res = results.subtests.find(t => t.name.startsWith(`${interfaceName} interface: ${member.type} ${member.name}`));
+              interfaces.results[interfaceName][member.name][browser.name] = res?.status || "N/A";
             }
-          }
         }
       }
     }
-    await fs.writeFile(shortname + ".json", JSON.stringify( interfaces, null, 2));
+    await fs.writeFile(trShortname + ".json", JSON.stringify( interfaces, null, 2));
   });
 })(shortnames).catch(err => console.error(err));
